9e850a7ed347b7de4ee59ebeee03d9b50dee206e
bestcoder12 <67689163+bestcoder12@users.noreply.github.com>
2023-04-19 18:39:56 +0530

    Change react-in-scope rule of eslint and rename App.js to App.jsx

--- code/report_automation_portal/frontend/src/App.jsx
+++ code/report_automation_portal/frontend/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/addReport" element={<AddReport />}></Route>
-          <Route path="/editReport" element={<EditReport />}></Route>
-          <Route path="/manageUsers" element={<ManageUsers />}></Route>
-          <Route path="/ChangePassword" element={<ChangePassword />}></Route>
-          <Route path="/logout" element={<Logout />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addReport" element={<AddReport />} />
+          <Route path="/editReport" element={<EditReport />} />
+          <Route path="/manageUsers" element={<ManageUsers />} />
+          <Route path="/ChangePassword" element={<ChangePassword />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </div>
     </Router>
